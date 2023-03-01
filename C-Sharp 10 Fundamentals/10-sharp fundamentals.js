4e37cffe94d3e4bbec661d7da670a7efb8e03208
livingsneaky-art <71326659+livingsneaky-art@users.noreply.github.com>
2023-03-01 23:31:56 +0800

    Create 10-sharp fundamentals.js

---
+++ C-Sharp 10 Fundamentals/10-sharp fundamentals.js
@@ -0,0 +1,17 @@
+//Course Overview
+/*
+    - Learning about C# 10 and its relation to .NET 6 and visual studio 2022
+    - Understanding the C# syntax
+    - Creating console applications and capturing user input
+    - Types and type-safety
+    - Basics of object-orientation and concepts such as inheritance and interfaces
+    - Creating unit test and Debugging the code
+
+    The end of the course You'll have a good understanding of the C# langauge
+
+    PREREQUISITES:
+    No prior Knowledge required
+*/
+
+//Getting Started with C# 10 and .NET 6
+
